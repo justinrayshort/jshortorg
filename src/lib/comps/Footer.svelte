3e6af7560795e48604b0b6e41fdeb0c9e9438fd7
Justin Short <justin@jshort.org>
2023-11-19 18:55:27 +0000

    updated navbar

--- src/lib/comps/Footer.svelte
+++ src/lib/comps/Footer.svelte
@@ -1,5 +1,5 @@
 <footer>
-	<footer class="footer p-10 bg-base-200 text-base-content">
+	<footer class="sticky bottom-0 footer p-10 bg-base-200 text-base-content">
 		<aside>
 			<svg
 				width="50"
---
+++ src/routes/tripd/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="js">
+</script>
+
+<div class="card w-96 glass">
+	<figure>
+		<img
+			class="mt-6"
+			src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+			alt="car!"
+		/>
+	</figure>
+	<div class="card-body">
+		<h2 class="card-title">Trip Calculator</h2>
+		<p>How much does your trip cost?</p>
+		<div class="card-actions justify-end">
+			<button class="btn btn-accent btn-outline">🧮 Calculate</button>
+		</div>
+	</div>
+</div>
